9b1ff1a350235621708502a6314c05debab4babb
H-Vidyasagar <86299037+H-Vidyasagar@users.noreply.github.com>
2021-10-22 12:33:00 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,11 @@
+class Example2
+{
+public static void main(String arg[])
+{
+int num;
+num=100;
+System.out.println("the value of num is "+num);
+num=num*2;
+System.out.println("the value of num*2 is "+num);
+}
+}
